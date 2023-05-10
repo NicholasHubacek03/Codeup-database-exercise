3a095689f519b2060fddda220eca346d1ce82dad
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-05-10 13:38:44 -0500

    Finsihed Where exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,7 +2,7 @@
 
 SELECT first_name
 FROM employees
-WHERE first_name or ('Irena', 'Vidya', 'Maya');
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
 SELECT * FROM employees
 where last_name like ('%E');
@@ -10,3 +10,15 @@ where last_name like ('%E');
 SELECT * FROM employees
 where first_name like ('%q');
 
+SELECT * FROM employees
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+  AND gender = 'M';
+
+SELECT * FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+SELECT * FROM employees
+WHERE last_name LIKE 'E%E';
+
+SELECT * FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
